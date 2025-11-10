20dc3c4d655701bd97c50bc34c09dcb0f150695d
JaeHyunGround <gothddlek@naver.com>
2025-11-10 23:00:10 +0900

    chore : HTML 포맷팅 적용

--- src/components/SearchFormBreadcrumb.js
+++ src/components/SearchFormBreadcrumb.js
@@ -1,28 +1,34 @@
 const category1Breadcrumb = ({ category1 }) => {
-  return /* html */ `
+  return /* HTML */ `
     <span class="text-xs text-gray-500">&gt;</span>
     <a href=${`/?category1=${category1}`}>
-      <button data-breadcrumb="category1" data-category1=${category1} class="text-xs hover:text-blue-800 hover:underline">${category1}</button>
+      <button
+        data-breadcrumb="category1"
+        data-category1=${category1}
+        class="text-xs hover:text-blue-800 hover:underline"
+      >
+        ${category1}
+      </button>
     </a>
-    `;
+  `;
 };
 
 const category2Breadcrumb = ({ category2 }) => {
-  return /* html */ `
-      <span class="text-xs text-gray-500">&gt;</span>
-      <span class="text-xs text-gray-600 cursor-default">${category2}</span>
-    `;
+  return /* HTML */ `
+    <span class="text-xs text-gray-500">&gt;</span>
+    <span class="text-xs text-gray-600 cursor-default">${category2}</span>
+  `;
 };
 
 export const SearchFormBreadcrumb = ({ params }) => {
   const category1 = params.get("category1");
   const category2 = params.get("category2");
 
-  return /* html */ `
-    <a href='/'>
+  return /* HTML */ `
+    <a href="/">
       <button data-breadcrumb="reset" class="text-xs hover:text-blue-800 hover:underline">전체</button>
     </a>
     ${category1 ? category1Breadcrumb({ category1 }) : ""}
     ${category1 && category2 ? category2Breadcrumb({ category2 }) : ""}
-    `;
+  `;
 };
--- src/pages/PageLayout.js
+++ src/pages/PageLayout.js
@@ -1,13 +1,11 @@
 import { Footer, Header } from "../components";
 
 export const PageLayout = ({ children }) => {
-  return /* html */ `
+  return /* HTML */ `
     <div class="min-h-screen bg-gray-50">
-        ${Header()}
-        <main class="max-w-md mx-auto px-4 py-4">
-            ${children}
-        </main>
-        ${Footer()}
+      ${Header()}
+      <main class="max-w-md mx-auto px-4 py-4">${children}</main>
+      ${Footer()}
     </div>
   `;
 };
