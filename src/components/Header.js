42e84ae2b22713856d452700be3e4a209d559414
JaeHyunGround <gothddlek@naver.com>
2025-11-13 01:09:46 +0900

    chore : 장바구니 카운트 span id 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ export const Header = () => {
 
     return /* HTML */ `
       <span
+        id="cart-count"
         class="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center"
       >
         ${storedCartData.length}
